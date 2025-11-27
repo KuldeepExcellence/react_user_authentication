4db5613d531e0ad007c35ea8bbe31585974526fb
KuldeepExcellence <95031853+KuldeepExcellence@users.noreply.github.com>
2025-11-27 17:51:29 +0530

    login form added

---
+++ login_singup_test_flow/src/login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+// import "./Login.css";
+import "./login.css";
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2 className="login-title">Login</h2>
+
+        <form onSubmit={handleSubmit} className="login-form">
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="login-btn">
+            Login
+          </button>
+        </form>
+
+        <p className="signup-text">
+          Don’t have an account? <span>Sign up</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
